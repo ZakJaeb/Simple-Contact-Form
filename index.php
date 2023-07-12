b44877d028c1b6a7d001fcf798f6837c9b9b560d
ZakJaeb <zakjaeb@gmail.com>
2023-07-12 19:26:14 -0400

    Added form.

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
     <title>Simple Contact Form</title>
 </head>
 <body>
-    <?php echo '<p>Hello World</p>'; ?>
+
+    <form action="welcome.php" method="post">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+</form>
 </body>
 </html>
